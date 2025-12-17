68ad801ff08c432eec927d4ff0636af737f3bac5
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-17 09:31:53 +0100

    * Adding validation and add method

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class TodoList extends Component
 {
+    #[Rule('required|min:3|string')]
+    public string $name;
+
+    public string $search;
+
+    public function add()
+    {
+        $validated = $this->validateOnly('name');
+        Todo::create($validated);
+        $this->reset('name');
+       session()->flash('success', 'Saved');
+
+    }
+    
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', ['todos' => Todo::paginate(5)]);
     }
 }
