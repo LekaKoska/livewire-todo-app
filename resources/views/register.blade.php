901882ef66a9e43a4cfed7d089d18888e0171138
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-18 13:34:55 +0100

    * Editing register blade with form * Adding image field in fillable * New route

---
+++ resources/views/register.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    @livewireScripts
+</head>
+<body>
+        <livewire:register-form/>
+
+@livewireStyles
+</body>
+</html>
